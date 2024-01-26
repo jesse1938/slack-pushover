a10fdeb4aaf31aa0852eef9bbaedc39b1d67a35d
jesse1938 <75123226+jesse1938@users.noreply.github.com>
2024-01-27 00:02:01 +0800

    fix import

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pushover_package',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'requests',
     ],
+    py_modules=['pushover'],
     
     description='A simple Python wrapper for the Pushover API',
     keywords='pushover notification api',
